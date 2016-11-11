feb432499a7bb656551c47422c99d6bf54f938d9
Utian Ayuba <utianayuba@gmail.com>
2016-11-11 10:03:21 +0700

    Create 03-nova.sh

---
+++ 03-nova.sh
@@ -0,0 +1,107 @@
+#!/bin/bash
+
+source os.conf
+source admin-openrc
+
+##### Nova Compute Service #####
+mysql -u root -p$PASSWORD -e "SHOW DATABASES;" | grep nova > /dev/null 2>&1 && echo "nova database exists"
+if [ $? -ne 0 ]
+  then
+    mysql -u root -p$PASSWORD -e "CREATE DATABASE nova; GRANT ALL PRIVILEGES ON nova.* TO 'nova'@'localhost' IDENTIFIED BY '$PASSWORD'; GRANT ALL PRIVILEGES ON nova.* TO 'nova'@'%' IDENTIFIED BY '$PASSWORD';"
+  fi
+
+mysql -u root -p$PASSWORD -e "SHOW DATABASES;" | grep nova_api > /dev/null 2>&1 && echo "nova_api database exists"
+if [ $? -ne 0 ]
+  then
+    mysql -u root -p$PASSWORD -e "CREATE DATABASE nova_api; GRANT ALL PRIVILEGES ON nova_api.* TO 'nova'@'localhost' IDENTIFIED BY '$PASSWORD'; GRANT ALL PRIVILEGES ON nova_api.* TO 'nova'@'%' IDENTIFIED BY '$PASSWORD';"
+  fi
+
+openstack user list | grep nova > /dev/null 2>&1 && echo "nova user exists"
+if [ $? -ne 0 ]
+  then
+    openstack user create --domain default --password $PASSWORD nova
+fi
+openstack role add --project service --user nova admin
+
+openstack service list | grep nova > /dev/null 2>&1 && echo "nova service exists"
+if [ $? -ne 0 ]
+  then
+    openstack service create --name nova --description "OpenStack Compute service" compute
+fi
+
+openstack endpoint list | grep public | grep nova > /dev/null 2>&1 && echo "nova public endpoint exists"
+if [ $? -ne 0 ]
+  then
+    openstack endpoint create --region RegionOne compute public http://$HOSTNAME:8774/v2.1/%\(tenant_id\)s
+fi
+
+openstack endpoint list | grep internal | grep nova > /dev/null 2>&1 && echo "nova internal endpoint exists"
+if [ $? -ne 0 ]
+  then
+    openstack endpoint create --region RegionOne compute internal http://$HOSTNAME:8774/v2.1/%\(tenant_id\)s
+fi
+
+openstack endpoint list | grep admin | grep nova > /dev/null 2>&1 && echo "nova admin endpoint exists"
+if [ $? -ne 0 ]
+  then
+    openstack endpoint create --region RegionOne compute admin http://$HOSTNAME:8774/v2.1/%\(tenant_id\)s
+fi
+zypper -n in --no-recommends openstack-nova-api openstack-nova-scheduler openstack-nova-conductor openstack-nova-consoleauth openstack-nova-novncproxy iptables
+
+[ ! -f /etc/nova/nova.conf.orig ] && cp -v /etc/nova/nova.conf /etc/nova/nova.conf.orig
+cat << _EOF_ > /etc/nova/nova.conf
+[DEFAULT]
+log_dir = /var/log/nova
+connection_type = libvirt
+compute_driver = libvirt.LibvirtDriver
+image_service = nova.image.glance.GlanceImageService
+volume_api_class = nova.volume.cinder.API
+auth_strategy = keystone
+bindir = /usr/bin
+state_path = /var/lib/nova
+service_neutron_metadata_proxy = True
+use_neutron = True
+enabled_apis = osapi_compute,metadata
+transport_url = rabbit://openstack:$PASSWORD@$HOSTNAME
+my_ip = $IPMAN
+firewall_driver = nova.virt.firewall.NoopFirewallDriver
+
+[api_database]
+connection = mysql+pymysql://nova:$PASSWORD@$HOSTNAME/nova_api
+
+[database]
+connection = mysql+pymysql://nova:$PASSWORD@$HOSTNAME/nova
+
+[keystone_authtoken]
+auth_uri = http://$HOSTNAME:5000
+auth_url = http://$HOSTNAME:35357
+memcached_servers = $HOSTNAME:11211
+auth_type = password
+project_domain_name = default
+user_domain_name = default
+project_name = service
+username = nova
+password = $PASSWORD
+
+[neutron]
+auth_type = password
+username = neutron
+password = %SERVICE_PASSWORD%
+user_domain_id = default
+project_name = %SERVICE_TENANT_NAME%
+project_domain_id = default
+
+[oslo_concurrency]
+lock_path = /var/run/nova
+
+[vnc]
+vncserver_listen = $my_ip
+vncserver_proxyclient_address = $my_ip
+
+[glance]
+api_servers = http://$HOSTNAME:9292
+_EOF_
+
+systemctl enable openstack-nova-api.service openstack-nova-consoleauth.service openstack-nova-scheduler.service openstack-nova-conductor.service openstack-nova-novncproxy.service
+systemctl restart openstack-nova-api.service openstack-nova-consoleauth.service openstack-nova-scheduler.service openstack-nova-conductor.service openstack-nova-novncproxy.service
+systemctl status openstack-nova-api.service openstack-nova-consoleauth.service openstack-nova-scheduler.service openstack-nova-conductor.service openstack-nova-novncproxy.service
